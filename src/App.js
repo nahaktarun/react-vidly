d74295387a9df786ceea9668ed0850113151a1d0
tarunkumarnahak25 <tarunnaha25@gmail.com>
2021-07-31 23:05:03 +0530

    added movie component with table with data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Movies from "./components/movies";
 
 function App() {
   return (
     <main className="container">
-      <h1>Hello world</h1>
+      <Movies></Movies>
     </main>
   );
 }
