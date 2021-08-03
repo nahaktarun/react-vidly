012c2f0151600779337860ceb47797eaf9e8877c
tarunkumarnahak25 <tarunnaha25@gmail.com>
2021-08-03 17:19:07 +0530

    added displaying items

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-const ListGroup = () => {
-  return null;
+const ListGroup = (props) => {
+  const { items, textProperty, valueProperty } = props;
+  return (
+    <React.Fragment>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item[valueProperty]} className="list-group-item">
+            {item[textProperty]}
+          </li>
+        ))}
+      </ul>
+    </React.Fragment>
+  );
 };
 
 export default ListGroup;
