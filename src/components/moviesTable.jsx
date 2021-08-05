bdf98845aeaa5677e9abfeb8b61a4acf9e97a3c8
tarunkumarnahak25 <tarunnaha25@gmail.com>
2021-08-05 12:33:11 +0530

    added extracting table into a table component

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import TableHeader from "./common/tableHeader";
+import Table from "./common/table";
 import Like from "./common/like";
-import TableBody from "./common/tableBody";
 
 class MoviesTable extends Component {
   columns = [
@@ -34,14 +33,12 @@ class MoviesTable extends Component {
   render() {
     const { movies, onSort, sortColumn } = this.props;
     return (
-      <table className="table">
-        <TableHeader
-          columns={this.columns}
-          sortColumn={sortColumn}
-          onSort={onSort}
-        ></TableHeader>
-        <TableBody columns={this.columns} data={movies}></TableBody>
-      </table>
+      <Table
+        columns={this.columns}
+        data={movies}
+        sortColumn={sortColumn}
+        onSort={onSort}
+      ></Table>
     );
   }
 }
