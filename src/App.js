634887f32df782a13cc7201a40ae66a73558709c
tarunkumarnahak25 <tarunnaha25@gmail.com>
2021-08-07 20:07:17 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from "react";
-import "./App.css";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Movies from "./components/movies";
 import Customers from "./components/cutomers";
 import Rentals from "./components/Rentals";
 import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import "./App.css";
 
 function App() {
   return (
-    <main className="container">
-      <Switch>
-        <Route path="/movies" component={Movies}></Route>
-        <Route path="/customers" component={Customers}></Route>
-        <Route path="/rentals" component={Rentals}></Route>
-        <Route path="/not-found" component={NotFound}></Route>
-        <Redirect from="/" exact to="/movies" />
-        <Redirect to="/not-found" />
-      </Switch>
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
