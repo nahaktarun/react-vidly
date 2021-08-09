0a91f7dbf99a3664ca0e48f90a58f4ab49a094b3
tarunkumarnahak25 <tarunnaha25@gmail.com>
2021-08-09 21:24:53 +0530

    added validation on change

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -32,10 +32,22 @@ class LoginForm extends Component {
 
     console.log("Submitted");
   };
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") return "username is required";
+    }
+    if (name === "password") {
+      if (value.trim() === "") return "Password is required";
+    }
+  };
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
   render() {
     const { account, errors } = this.state;
